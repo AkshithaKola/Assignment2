20e1bb507c04383baa25935c5220ac6d006db795
AkshithaKola <102524785+AkshithaKola@users.noreply.github.com>
2022-04-18 14:09:06 +0530

    Update main2.py

--- code/main2.py
+++ code/main2.py
@@ -8,3 +8,16 @@ y=1/mp.sin(a)
 #computing the given equation
 z=(1/(x*x))+(1/(y*y))
 print(z)
+
+#plotting graph
+a=np.arange(0.001,1.57,0.01)
+x=1/np.cos(a)
+y=1/np.sin(a)
+z=(1/(x*x))+(1/(y*y))
+plt.grid()
+plt.plot(a,z,color='green',label = 'return values of the given function')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.legend(loc="upper left")
+
+plt.show()
